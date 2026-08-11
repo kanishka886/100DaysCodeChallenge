a2ae5a7d4965078613ae8920e172fd64768d9353
kanishka886 <kanishka.32886@stu.upes.ac.in>
2026-08-11 23:00:00 +0530

    Added Day 2 solutions

---
+++ Question_3_Day 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+float r;
+
+ printf("what is the radius of your circle?");
+ scanf ("%f", &r ); 
+ printf("The area of the circle is %f and the circumfrence is %f", 3.14*r*r, 2*3.14*r);
+	
+	return 0;
+}
---
+++ Question_4_Day 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+int b, l;
+ printf("what is the lenght and breadth of your rectangle?");
+ scanf ("%d %d", &l, &b );
+
+ printf("The area of the rectangle is %d and the parameter is %d", l*b, 2*(l+b));
+
+	
+	return 0;
+}
