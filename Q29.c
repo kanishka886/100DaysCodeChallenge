8e52a3be3266597aa43634df69c525c9e36fcfcd
kanishka886 <kanishka.32886@stu.upes.ac.in>
2026-08-24 23:00:00 +0530

    Added Day 15 solutions

---
+++ Q29.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Solution for Q29
+");
+    return 0;
+}
---
+++ Q30.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Solution for Q30
+");
+    return 0;
+}
