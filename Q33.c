83b841d79dbbf75ab85aa87384a9730ee82d821f
kanishka886 <kanishka.32886@stu.upes.ac.in>
2026-08-26 23:00:00 +0530

    Added Day 17 solutions

---
+++ Q33.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Solution for Q33
+");
+    return 0;
+}
---
+++ Q34.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Solution for Q34
+");
+    return 0;
+}
