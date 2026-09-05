7da9c56b5ef514abbaf2d59b0bd34ab3583b5a3c
kanishka886 <kanishka.32886@stu.upes.ac.in>
2026-09-05 23:00:00 +0530

    Added Day 27 solutions

---
+++ Q53.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+
+int main(void)
+{
+	for (int i = 1; i <= 5; i++) {
+		for (int j = 0; j < 2 * i - 1; j++)
+			putchar('*');
+		putchar('\n');
+	}
+
+	for (int i = 4; i >= 1; i--) {
+		for (int j = 0; j < 2 * i - 1; j++)
+			putchar('*');
+		putchar('\n');
+	}
+
+	return 0;
+}
